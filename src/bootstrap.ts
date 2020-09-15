54b61f653f5966cda51f5e46c6cfae1d8df5b302
Daniil <zelenko.danil@gmail.com>
2020-09-15 20:51:40 +0300

    get all handler obj from controllers

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -7,9 +7,21 @@ export class Bootstrap {
         this.controllers = controllers;
     }
 
+    getAllHandlersFromControllers() {
+        let handlers = [];
+        for(let i = 0; i < this.controllers.length; i++) {
+            let metaKeys = Reflect.getMetadataKeys(this.controllers[i]);
+            for (let k = 0; k < metaKeys.length; k++) {
+                handlers.push(Reflect.getMetadata(metaKeys[k], this.controllers[i]));
+            }
+        }
+        return handlers;
+    }
+
     start(expressApp: Application) {
 
         expressApp.all("*", (req: Request, res: Response) => {
+            let handlers = this.getAllHandlersFromControllers();
 
         })
 
