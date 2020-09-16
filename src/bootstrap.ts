8896c36862227553489e9e04bb460687e9c0d5a5
Daniil <zelenko.danil@gmail.com>
2020-09-16 19:08:20 +0300

    Write type for handler obj. Bind handle func to controller. Init getHandler func

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,5 +1,11 @@
 import {Application, Request, Response} from "express";
 
+type handler = {
+    method: string,
+    path: string,
+    handleFunc: (req: Request) => any
+}
+
 export class Bootstrap {
     controllers: any[];
 
@@ -7,22 +13,29 @@ export class Bootstrap {
         this.controllers = controllers;
     }
 
-    getAllHandlersFromControllers() {
+    getAllHandlersFromControllers(): handler[] {
         let handlers = [];
         for(let i = 0; i < this.controllers.length; i++) {
             let metaKeys = Reflect.getMetadataKeys(this.controllers[i]);
             for (let k = 0; k < metaKeys.length; k++) {
-                handlers.push(Reflect.getMetadata(metaKeys[k], this.controllers[i]));
+                let handler: handler = Reflect.getMetadata(metaKeys[k], this.controllers[i]);
+                handler.handleFunc = handler.handleFunc.bind(this.controllers[k]);
+                handlers.push(handler);
             }
         }
         return handlers;
+    }
+
+    getHandler(url: string, handlers: handler[]) {
+
     }
 
     start(expressApp: Application) {
 
         expressApp.all("*", (req: Request, res: Response) => {
             let handlers = this.getAllHandlersFromControllers();
-
+            let url = req.url;
+            let handler = this.getHandler(url, handlers);
         })
 
     }
