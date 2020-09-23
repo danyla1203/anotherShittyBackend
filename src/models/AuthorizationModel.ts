2b54368925437568bd2722ad96239b8afae70768
Daniil <zelenko.danil@gmail.com>
2020-09-23 21:33:25 +0300

    Interface for repo. Verify user init

--- src/models/AuthorizationModel.ts
+++ src/models/AuthorizationModel.ts
@@ -1,12 +1,27 @@
-interface AuthorizationRepositoryI {
+import {BadPassword, NoSuchUser} from "../lib/Error";
 
+export interface AuthorizationRepositoryI {
+    findUser(name: string): any
 }
 
 export class AuthorizationModel {
-    repository: AuthorizationRepositoryI;
+    authRepository: AuthorizationRepositoryI;
+
     constructor(repo: AuthorizationRepositoryI) {
-        this.repository = repo;
+        this.authRepository = repo;
     }
 
+    verifyUserLogin(userName: string, password: string) {
+        let user = this.authRepository.findUser(userName);
+        if (user) {
+            if (user.password == password) {
+                //create session
+            } else {
+                throw new BadPassword()
+            }
 
+        } else {
+            throw new NoSuchUser();
+        }
+    }
 }
