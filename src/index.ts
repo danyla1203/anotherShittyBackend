c2ab9a95fcdde3194d38716289dd238f5e6676f2
Daniil <zelenko.danil@gmail.com>
2020-09-23 18:52:36 +0300

    redis connection. Pass article controller in boostrap

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 import * as express from "express";
-import { Bootstrap } from "./bootstrap";
+import * as redis from "redis";
+
+import {Bootstrap} from "./bootstrap";
+import {ArticleController} from "./controllers/ArticleController";
 
 const app = express();
 
-const controllers = [
+//create connection to data storage
+export const redisClient = redis.createClient();
+redisClient.on("error", (error) => {
+    console.error(error);
+});
+export const dbConnection = null;
 
+//put new controllers here
+const controllers = [
+    new ArticleController()
 ];
 const bootstrap = new Bootstrap(controllers);
 
