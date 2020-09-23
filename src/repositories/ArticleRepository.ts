6a237c211d20f27d9a54abd1ba0491684794003d
Daniil <zelenko.danil@gmail.com>
2020-09-23 17:58:52 +0300

    write empty articles repo

---
+++ src/repositories/ArticleRepository.ts
@@ -0,0 +1,10 @@
+import {ArticleRepositoryI} from "../models/ArticleModel";
+
+export class ArticleRepository implements ArticleRepositoryI {
+    findArticle(article_id: number) {
+        return null;
+    }
+    findUserArticles(user_id: number) {
+        return null;
+    }
+}
