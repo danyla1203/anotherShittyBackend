672e6a6aa98f86c65f82b3ac2a2187a54bff1e5a
Daniil <zelenko.danil@gmail.com>
2020-09-24 18:48:08 +0300

    create session method. Get out hash name in field

--- src/repositories/AuthorizationRepository.ts
+++ src/repositories/AuthorizationRepository.ts
@@ -1,14 +1,25 @@
 import {RedisClient} from "redis";
+import * as crypto from "crypto";
+
 import {redisClient} from "../index";
 import {AuthorizationRepositoryI} from "../models/AuthorizationModel";
 
-export class AuthorizationRepository implements AuthorizationRepositoryI{
+export class AuthorizationRepository implements AuthorizationRepositoryI {
     connection: RedisClient = redisClient;
+    hashName: string = "users_sessions";
+
     constructor() {
         //if no user_sessions hash -> create new hash;
-        if (!this.connection.exists("user_sessions")) {
-            this.connection.hset("user_sessions");
+        if (!this.connection.exists(this.hashName)) {
+            this.connection.hset(this.hashName);
         }
+    }
+
+    createSession(ip: string, userAgent: string) {
+        let session_id = crypto.createHash("sha256")
+                                .update(ip + userAgent)
+                                .digest("hex");
+        this.connection.hsetnx(this.hashName, session_id, "{}");
     }
 
     findUser(userName: string) {
