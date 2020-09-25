4af90eae4b7fea43793eaa96b6a792d66336d2d4
Daniil <zelenko.danil@gmail.com>
2020-09-25 18:56:23 +0300

    users sessions doesn't store in one big hash

--- src/repositories/AuthorizationRepository.ts
+++ src/repositories/AuthorizationRepository.ts
@@ -6,20 +6,12 @@ import {AuthorizationRepositoryI} from "../models/AuthorizationModel";
 
 export class AuthorizationRepository implements AuthorizationRepositoryI {
     connection: RedisClient = redisClient;
-    hashName: string = "users_sessions";
-
-    constructor() {
-        //if no user_sessions hash -> create new hash;
-        if (!this.connection.exists(this.hashName)) {
-            this.connection.hset(this.hashName);
-        }
-    }
 
     createSession(ip: string, userAgent: string) {
         let session_id = crypto.createHash("sha256")
                                 .update(ip + userAgent)
                                 .digest("hex");
-        this.connection.hsetnx(this.hashName, session_id, "{}");
+        this.connection.hset(session_id, "{}");
     }
 
     findUser(userName: string) {
