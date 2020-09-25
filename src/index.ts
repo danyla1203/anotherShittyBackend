be52189639e1a2d6a743e0098c626a3310da6f9e
Daniil <zelenko.danil@gmail.com>
2020-09-25 18:54:57 +0300

    change redis client

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import * as express from "express";
-import * as redis from "redis";
+import * as redis from "promise-redis";
 
 import {Bootstrap} from "./bootstrap";
 import {ArticleController} from "./controllers/ArticleController";
+import {AuthorizationRepository} from "./repositories/AuthorizationRepository";
 
 const app = express();
 
 //create connection to data storage
-export const redisClient = redis.createClient();
+export const redisClient = redis().createClient();
 redisClient.on("error", (error) => {
     console.error(error);
 });
@@ -22,3 +23,7 @@ const bootstrap = new Bootstrap(controllers);
 bootstrap.start(app);
 
 app.listen(3000);
+
+
+const test = new AuthorizationRepository();
+test.findUser("bitch");
