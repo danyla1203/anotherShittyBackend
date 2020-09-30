8962e5cec90d48ce72e7c75b9d4dfcad66c2bb43
Daniil <zelenko.danil@gmail.com>
2020-09-30 20:55:39 +0300

    postgres driver

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as express from "express";
 import * as redis from "promise-redis";
+import {Client} from "pg";
 
 import {Bootstrap} from "./bootstrap";
 import {ArticleController} from "./controllers/ArticleController";
@@ -12,7 +13,19 @@ export const redisClient = redis().createClient();
 redisClient.on("error", (error) => {
     console.error(error);
 });
-export const dbConnection = null;
+export const dbConnection = new Client({
+    user: "postgres",
+    host: "localhost",
+    database: "test",
+    password: "root",
+});
+dbConnection.connect((err) => {
+    if (err) {
+        console.error('connection error', err.stack)
+    } else {
+        console.log('connected')
+    }
+});
 
 //put new controllers here
 const controllers = [
