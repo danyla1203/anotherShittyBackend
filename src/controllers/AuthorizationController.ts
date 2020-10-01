0b804c9179685789c0e3a81f03ed65c560164268
Daniil <zelenko.danil@gmail.com>
2020-10-01 14:07:19 +0300

    get tokens

--- src/controllers/AuthorizationController.ts
+++ src/controllers/AuthorizationController.ts
@@ -1,4 +1,4 @@
-import {Request} from "express";
+import {Request, Response} from "express";
 
 import {Controller} from "../lib/ControllerDecorator";
 import {AuthorizationModel} from "../models/AuthorizationModel";
@@ -14,12 +14,16 @@ export class AuthorizationController {
     authModel: AuthorizationModel;
 
     @post("/login")
-    async login(req: Request) {
+    async login(req: Request, res: Response) {
         let userName = req.body["name"];
         let password = req.body["password"];
 
         this.authModel.verifyUserLogin(userName, password);
         let session_id = this.authModel.createSession(req.ip, req.headers["user-agent"]);
-        //TODO: get tokens and set they in cookies.
+        let [ accessToken, refreshToken ] = this.authModel.getTokens(session_id, userName);
+
+        res.cookie("s_id", session_id,  { httpOnly: true });
+        res.cookie("refresh_token", refreshToken, {  maxAge: 9000000, httpOnly: true });
+        return { accessToken: accessToken };
     }
 }
