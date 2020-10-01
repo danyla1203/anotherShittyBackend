0b80c30ec8f832e8cfdd48b28c482fe3a6041f48
Daniil <zelenko.danil@gmail.com>
2020-10-01 14:06:21 +0300

    All tokens and session_id generate in model. Get tokens

--- src/models/AuthorizationModel.ts
+++ src/models/AuthorizationModel.ts
@@ -1,8 +1,10 @@
 import {BadPassword, NoSuchUser} from "../lib/Error";
+import * as crypto from "crypto";
 
 export interface AuthorizationRepositoryI {
     findUserFromDb(name: string): any
-    createSession(ip: string, userAgent: string): string
+    createSession(session_id: string): void
+    setTokens(session_id: string, access: string, refresh: string): void;
 }
 
 export class AuthorizationModel {
@@ -12,8 +14,31 @@ export class AuthorizationModel {
         this.authRepository = repo;
     }
 
+    createSeesionId(ip: string, userAgent: string): string {
+        let session_id = crypto.createHash("sha256")
+            .update(ip + userAgent)
+            .digest("hex");
+        return session_id
+    }
+
+    createAccesToken(userName: string): string {
+        return ""
+    }
+    createRefreshToken(): string {
+        return ""
+    }
+
+    getTokens(session_id: string, userName: string): string[] {
+        let accessToken = this.createAccesToken(userName);
+        let refreshToken = this.createRefreshToken();
+        this.authRepository.setTokens(session_id, accessToken, refreshToken);
+        return [ accessToken, refreshToken ];
+    }
+
     createSession(ip: string, userAgent: string): string {
-        return this.authRepository.createSession(ip, userAgent);
+        let sessionId = this.createSeesionId(ip, userAgent);
+        this.authRepository.createSession(sessionId);
+        return sessionId;
     }
 
     async verifyUserLogin(userName: string, password: string) {
