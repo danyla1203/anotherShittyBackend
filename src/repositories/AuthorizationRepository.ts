10ed5451ee0c3f5595ba382b64dd9e08b567f41e
Daniil <zelenko.danil@gmail.com>
2020-10-04 13:19:38 +0300

    find user types

--- src/repositories/AuthorizationRepository.ts
+++ src/repositories/AuthorizationRepository.ts
@@ -1,7 +1,13 @@
 import {RedisClient} from "redis";
-import {Client} from "pg";
+import {Client, QueryResult} from "pg";
 
 import {AuthorizationRepositoryI} from "../models/AuthorizationModel";
+
+export type User = {
+    user_id: number;
+    name: string,
+    password: string
+}
 
 export class AuthorizationRepository implements AuthorizationRepositoryI {
     redis: RedisClient;
@@ -16,8 +22,9 @@ export class AuthorizationRepository implements AuthorizationRepositoryI {
         this.redis.hset(session_id, "{}");
     }
 
-    async findUserFromDb(userName: string) {
-        let user = await this.database.query(`select * from users where name=${userName}`);
+    async findUserFromDb(userName: string): Promise<User | null> {
+        let result: QueryResult = await this.database.query(`select * from users where name=${userName}`);
+        let user: User = result.rows[0];
         if (user) {
             return user;
         } else {
