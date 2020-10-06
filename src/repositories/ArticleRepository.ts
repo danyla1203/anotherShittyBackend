4c75c92b363cc28ec0c2431b7c56a149a1a100ee
Daniil <zelenko.danil@gmail.com>
2020-10-06 19:57:26 +0300

    db connection insert, find article by article_id

--- src/repositories/ArticleRepository.ts
+++ src/repositories/ArticleRepository.ts
@@ -1,9 +1,31 @@
 import {ArticleRepositoryI} from "../models/ArticleModel";
+import {Client} from "pg";
+
+type Article = {
+    article_id: number;
+}
 
 export class ArticleRepository implements ArticleRepositoryI {
-    findArticle(article_id: number) {
-        return null;
+    db: Client;
+    constructor(db: Client) {
+        this.db = db;
     }
+
+    async findArticle(article_id: number): Promise<Article | null> {
+        try {
+            let result = await this.db.query(`select * from articles where article_id='${article_id}'`);
+            let article: Article = result.rows[0];
+            if (article) {
+                return article
+            } else {
+                return null
+            }
+        } catch (e) {
+            console.log(e.error);
+            return null;
+        }
+    }
+
     findUserArticles(user_id: number) {
         return null;
     }
