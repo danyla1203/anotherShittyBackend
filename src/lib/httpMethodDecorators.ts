4949a233a35f45fd2b20541cdfe88652243f52e9
Daniil <zelenko.danil@gmail.com>
2020-10-08 19:27:51 +0300

    delete, put method

--- src/lib/httpMethodDecorators.ts
+++ src/lib/httpMethodDecorators.ts
@@ -28,3 +28,34 @@ export function post(path: string) {
         Reflect.defineMetadata(path, handler, target);
     }
 }
+
+export function Delete(path: string) {
+    return function(
+        target: any,
+        methodName: string,
+        funcDescriptor: any
+    ) {
+        let handler = {
+            method: "DELETE",
+            path: path,
+            handlerFunc: funcDescriptor.value
+        };
+        Reflect.defineMetadata(path, handler, target);
+    }
+}
+
+export function put(path: string) {
+    return function(
+        target: any,
+        methodName: string,
+        funcDescriptor: any
+    ) {
+        let handler = {
+            method: "PUT",
+            path: path,
+            handlerFunc: funcDescriptor.value
+        };
+        Reflect.defineMetadata(path, handler, target);
+    }
+}
+
