6527ea4812c584fec5a62138236a230d2369c41f
Daniil <zelenko.danil@gmail.com>
2020-10-21 20:32:16 +0300

    extend test

--- test/bootstrap.test.ts
+++ test/bootstrap.test.ts
@@ -1,10 +1,20 @@
-import { Request } from "express";
+import {Request, Response} from "express";
 import {Bootstrap} from "../src/bootstrap";
 
 const bootstrapTest = new Bootstrap([]);
 
 test("handler", () => {
     let handlers = [
+        {
+            method: "GET",
+            path: "/articles/:user_id",
+            handlerFunc: (req: Request) => {}
+        },
+        {
+            method: "GET",
+            path: "/articles",
+            handlerFunc: (req: Request) => {}
+        },
         {
             method: "POST",
             path: "/",
@@ -27,7 +37,8 @@ test("handler", () => {
         }
     ];
 
-    expect(bootstrapTest.getHandler("/", "POST", handlers)).toBe(handlers[0]);
-    expect(bootstrapTest.getHandler("/article/12", "POST", handlers)).toBe(handlers[3]);
-    expect(bootstrapTest.getHandler("/test/12/23", "GET", handlers)).toBe(handlers[2])
+    expect(bootstrapTest.getHandler("/", "POST", handlers)).toBe(handlers[2]);
+    expect(bootstrapTest.getHandler("/articles", "GET", handlers)).toBe(handlers[1]);
+    expect(bootstrapTest.getHandler("/article/12", "POST", handlers)).toBe(handlers[5]);
+    expect(bootstrapTest.getHandler("/test/12/23", "GET", handlers)).toBe(handlers[4])
 });
