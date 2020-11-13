dee67e487b19a21b855b6ef4165c53600fce700b
Daniil <zelenko.danil@gmail.com>
2020-11-13 13:51:48 +0200

    improve test

--- test/bootstrap.test.ts
+++ test/bootstrap.test.ts
@@ -1,45 +1,44 @@
 import {Request, Response} from "express";
-import {Bootstrap} from "../src/bootstrap";
+import {Bootstrap, handler} from "../src/bootstrap";
 import {PostBody} from "../src/lib/PostBody";
 
 const bootstrapTest = new Bootstrap([], new PostBody());
-
-test("handler", () => {
-    let handlers = [
+describe("testing get handler func", () => {
+    let handlerFunc = jest.fn();
+    let handlers: handler[] = [
         {
             method: "GET",
             path: "/articles/:user_id",
-            handlerFunc: (req: Request) => {}
+            handlerFunc: handlerFunc
         },
         {
             method: "GET",
             path: "/articles",
-            handlerFunc: (req: Request) => {}
-        },
-        {
-            method: "POST",
-            path: "/",
-            handlerFunc: (req: Request) => {}
+            handlerFunc: handlerFunc
         },
         {
             method: "GET",
             path: "/",
-            handlerFunc: (req: Request) => {}
+            handlerFunc: handlerFunc
         },
         {
             method: "GET",
             path: "/test/:id/:user_id",
-            handlerFunc: (req: Request) => {}
+            handlerFunc: handlerFunc
         },
         {
             method: "POST",
             path: "/article/:path",
-            handlerFunc: (req: Request) => {}
+            handlerFunc: handlerFunc
         }
     ];
-
-    expect(bootstrapTest.getHandler("/", "POST", handlers)).toBe(handlers[2]);
-    expect(bootstrapTest.getHandler("/articles", "GET", handlers)).toBe(handlers[1]);
-    expect(bootstrapTest.getHandler("/article/12", "POST", handlers)).toBe(handlers[5]);
-    expect(bootstrapTest.getHandler("/test/12/23", "GET", handlers)).toBe(handlers[4])
+    test("test simple rout", () => {
+        expect(bootstrapTest.getHandler("/", "POST", handlers)).toBe(handlers[2]);
+        expect(bootstrapTest.getHandler("/articles", "GET", handlers)).toBe(handlers[1]);
+    });
+    test("test routs with pattern", () => {
+        expect(bootstrapTest.getHandler("/article/12", "POST", handlers)).toBe(handlers[5]);
+        expect(bootstrapTest.getHandler("/test/12/23", "GET", handlers)).toBe(handlers[4]);
+        expect(bootstrapTest.getHandler("/articles/1", "GET", handlers)).toBe(handlers[0]);
+    });
 });
